e25a9dc101f16defe24d8f21e22f7c9f47db1f91
MAHDI HASAN SHUVO <102132190+Mahdi-hasan-shuvo@users.noreply.github.com>
2025-08-20 19:11:26 +0600

    Update problem_1.c

--- CSE-62/Physics/Group_Assignment/problem_1.c
+++ CSE-62/Physics/Group_Assignment/problem_1.c
@@ -1,29 +1,91 @@
-#include <stdio.h>
-#include <math.h>
+
+#include<stdio.h>
+#include<math.h>
+#define pi 3.1416
 // Name : Mahdi Hasan Shuvo
 // GitHub: Mahdi-Hasan-Shuvo
 // ID: 251-115-030 
-int main() {
-	double theta, lamda,d;
-	int m;
-	printf("Enter Wavelength Lambda (NM): ");
-    scanf("%lf", &lamda);
-    if (lamda < 380 || lamda > 750) {
-        printf("Out of the range. Please enter a valid number.\n");
-       // main();
+int main()
+{
+    double lemda_nm,lemda_m,m,a1,a2,a3,t1,t2,t3,sint;
+    printf("Enter wavelength in nm : ");
+    scanf("%lf",&lemda_nm);
+    if(lemda_nm < 380 || lemda_nm >750)
+    {
+        printf("Out of Range\n",lemda_nm);
     }
-    printf("Enter angle θ in degrees : ");
-    scanf("%lf", &theta);
+    printf("Enter the value of m : ");
+    scanf("%lf",&m);
 
-    printf("Enter slit separation : ");
-    scanf("%lf", &d);
-    double lamda_m = lamda * 1e-9;      // nm to meter
-    double d_m = d * 1e-6;                // µm to meter
-    double theta_radious = theta * (3.1416 / 180); // degrees to radians
-    double calculation = (d_m * sin(theta_radious)) / lamda_m;
-    m =(int)(calculation + 0.5);
-    printf("%d-th order maxima\n", m);
+    printf("Enter the first slit width a1 : ");
+    scanf("%lf",&a1);
+    printf("Enter the second slit width a2 : ");
+    scanf("%lf",&a2);
+    printf("Enter the third slit width a3 : ");
+    scanf("%lf",&a3);
 
+
+    lemda_m = lemda_nm * 1e-9;
+
+    sint = (m * lemda_m) / (a1 * 1e-6);
+    t1 = asin(sint) * (180 / pi);
+
+    sint = (m * lemda_m) / (a2 * 1e-6);
+    t2 = asin(sint) * (180 / pi);
+
+    sint = (m * lemda_m) / (a3 * 1e-6);
+    t1 = asin(sint) * (180 / pi);
+
+    printf("The angle for slit a1 :%.2f degrees\n",t1);
+    printf("The angle for slit a2 :%.2f degrees\n",t2);
+    printf("The angle for slit a3 :%.2f degrees\n",t3);
+
+
+    if(t1>t2&&t1>t3)
+    {
+        printf("slit a1 light bend the most\n");
+    }
+    else if(t2 > 1 && t2 > t3)
+    {
+        printf("slit a2 light bend the most\n");
+    }
+    else if(t3 > t1 && t3 > t2)
+    {
+        printf("slit a3 light bend the most\n");
+    }
+
+    printf("color of light : ");
+    if(lemda_nm >=380 && lemda_nm < 450)
+    {
+        printf("Violet\n");
+    }
+    else if(lemda_nm >= 450 && lemda_nm < 485)
+    {
+        printf("Blue\n");
+    }
+    else if(lemda_nm >=485 && lemda_nm < 500)
+    {
+        printf("Cyan\n");
+    }
+    else if(lemda_nm >=500 && lemda_nm < 565)
+    {
+        printf("Green\n");
+    }
+    else if(lemda_nm >=565 && lemda_nm < 590)
+    {
+        printf("Yellow\n");
+    }
+    else if(lemda_nm >=590 && lemda_nm < 625)
+    {
+        printf("Orange\n");
+    }
+    else if(lemda_nm >=625 && lemda_nm < 750)
+    {
+        printf("Red\n");
+    }
+    else
+    {
+        printf("Invalid Input");
+    }
     return 0;
-
 }
